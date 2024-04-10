8a49820266a2a0993231a1e99aa629c123d52adc
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-10 22:19:36 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -298,3 +298,14 @@ for (const folder of triggerFolders) {
 // Login into your client application with bot's token.
 
 client.login(token);
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
