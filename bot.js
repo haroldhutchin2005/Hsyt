c59ab673f6c7db42ac00085de910f6b5eda9f305
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-10 22:22:19 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -306,6 +306,8 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
